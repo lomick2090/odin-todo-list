5b768e7b71f13886e1c5b7290cc3422883cee5d2
minkman <lomick2090@gmail.com>
2022-12-24 12:33:13 -0600

    Add Edit button

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ function populateLists() {
         let newListDiv = document.createElement('div');
         let newList = document.createElement('li');
         newListDiv.className = 'listnames'
-        newListDiv.id = `${i}`
+        newListDiv.id = i
         newList.textContent = taskControl.lists[i].name;
 
         let newListButton = document.createElement('button');
@@ -53,6 +53,81 @@ function populateLists() {
     }
 }
 
+function deleteTask(listIndex, taskIndex) {
+    taskControl.lists[listIndex].tasks.splice(taskIndex, 1);
+    populateTasks(listIndex);
+}
+
+function editTask(listIndex, taskIndex) {
+    let taskFormDiv = document.createElement('div')
+    taskFormDiv.className = 'taskformdiv';
+
+    let taskForm = document.createElement('form');
+    taskForm.className = 'taskform';
+
+    let taskHeader = document.createElement('h1');
+    taskHeader.textContent = 'Edit Task:';
+
+    let taskNameInputLabel = document.createElement('label');
+    taskNameInputLabel.for = 'taskname'
+    taskNameInputLabel.textContent = 'Task Name: '
+
+    let taskNameInput = document.createElement('input');
+    taskNameInput.type = 'text';
+    taskNameInput.id = 'taskname';
+    taskNameInput.name = 'taskname';
+    taskNameInputLabel.appendChild(taskNameInput)
+
+    let taskDescInputLabel = document.createElement('label');
+    taskDescInputLabel.for = 'taskdesc'
+    taskDescInputLabel.textContent = 'Task Description: '
+
+    let taskDescInput = document.createElement('input');
+    taskDescInput.type = 'text';
+    taskDescInput.id = 'taskdesc';
+    taskDescInput.name = 'taskdesc';
+    taskDescInputLabel.appendChild(taskDescInput)
+
+    let taskDueInputLabel = document.createElement('label');
+    taskDueInputLabel.for = 'taskdue'
+    taskDueInputLabel.textContent = 'Task Due Date: '
+
+    let taskDueInput = document.createElement('input');
+    taskDueInput.type = 'date';
+    taskDueInput.id = 'taskdue';
+    taskDueInput.name = 'taskdue';
+    taskDueInputLabel.appendChild(taskDueInput)
+
+    let submitButton = document.createElement('button');
+    submitButton.textContent = 'Add';
+    submitButton.addEventListener('click', () => {
+        event.preventDefault();
+        console.log(listIndex)
+        console.log(taskIndex)
+        console.log(taskControl.lists[listIndex].tasks[taskIndex])
+        taskControl.lists[listIndex].tasks[taskIndex].name = taskNameInput.value;
+        taskControl.lists[listIndex].tasks[taskIndex].desc = taskDescInput.value
+        taskControl.lists[listIndex].tasks[taskIndex].due = taskDueInput.value
+        populateTaskPage(listIndex);
+        taskForm.reset();
+        document.querySelector('.page').id = '';
+        taskFormDiv.remove();
+    });
+
+
+
+    taskFormDiv.appendChild(taskHeader);
+    taskForm.appendChild(taskNameInputLabel);
+    taskForm.appendChild(taskDescInputLabel);
+    taskForm.appendChild(taskDueInputLabel);
+    taskForm.appendChild(submitButton);
+    taskFormDiv.appendChild(taskForm);
+
+    document.querySelector('body').appendChild(taskFormDiv);
+
+    document.querySelector('.page').id = 'blur';
+}
+
 function populateTasks(listIndex) {
     while (document.querySelector('.tasks')) {
         document.querySelector('.tasks').remove()
@@ -61,6 +136,7 @@ function populateTasks(listIndex) {
 
     for (let i = 0; i < (taskControl.lists[listIndex].tasks.length); i++) {
         let newTaskDiv = document.createElement('div');
+        newTaskDiv.id = i;
         newTaskDiv.className = 'tasks'
 
         let newTaskHeader = document.createElement('h3');
@@ -74,6 +150,22 @@ function populateTasks(listIndex) {
         let newTaskDue = document.createElement('p');
         newTaskDue.textContent = `Due by: ${taskControl.lists[listIndex].tasks[i].due}`;
         newTaskDiv.appendChild(newTaskDue);
+
+        let newTaskButton = document.createElement('button');
+        newTaskButton.textContent = 'X';
+        newTaskButton.className = 'taskDeleteButton';
+        newTaskButton.addEventListener('click', () => deleteTask(listIndex, i));
+
+        let taskEditButton = document.createElement('button');
+        taskEditButton.textContent = 'Edit';
+        taskEditButton.className = 'taskEditButton';
+        taskEditButton.addEventListener('click', () => editTask(listIndex, i));
+
+        let buttonDiv = document.createElement('div');
+        buttonDiv.className = 'buttondiv';
+        buttonDiv.appendChild(taskEditButton);
+        buttonDiv.appendChild(newTaskButton);
+        newTaskDiv.appendChild(buttonDiv);
 
         document.querySelector('.list').appendChild(newTaskDiv);
 
